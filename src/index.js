30fbf7cec3e8573dbe9df4a26a7c0c251d0801b6
Vlad Rindevich <rindevich.vs@gmail.com>
2017-03-27 04:16:36 +0300

    feat: add source export

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export {default as applyMiddleware} from './applyMiddleware';
+export {associateActions} from './associatedActions';
+export {default as combineReducers} from './combineReducers';
+export {default as compose} from './compose';
+export {default as createStore} from './createStore';
